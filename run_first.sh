ad7664f3aeb14c0fe6d06c71c82de2acf3a945f9
standrs <stganders@gmail.com>
2017-03-25 15:05:33 -0400

    add music and crystal

--- run_first.sh
+++ run_first.sh
@@ -3,11 +3,12 @@
 echo "Installing software..."
 echo "Homebrew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-echo "oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo "Brewing a bundle..."
 brew tap Homebrew/bundle
 brew bundle
+
+echo "oh-my-zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Set up zshrc
 cp .zshrc ~/.zshrc
