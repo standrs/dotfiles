54bfb8bac5eb116abc796da43e916af487fe27ed
Stephen Anders <stephen.anders@shopify.com>
2021-02-12 14:53:07 -0500

    change order of plugin installation

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 sudo apt-get install -y ripgrep fd-find fzf neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-nvim +PlugInstall +qall
 
 if [ ! $ZSH ]; then
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -14,6 +13,8 @@ cp ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/vimrc ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/zshrc.d ~/.zshrc.d
+
+nvim +PlugInstall +qall
 
 source ~/.zshrc
 
